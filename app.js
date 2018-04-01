5608b80c09cf65a4c4fa3009ec12649c034ed34f
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-31 22:40:59 -0600

    some updates, delete needs fixing

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-$(function() {
-	$("#form-inline").on("submit", function(e) {
+$(document).ready(function() {
+	$(".form").on("submit", function(e) {
 		e.preventDefault();
-		var textEntry = $("#textbox").val();
-		$("ul").append("<li>" + textEntry + "</li>");
-	
-		})
-})
+		var textEntry = $(".textbox").val();
+		$("#listItems").prepend($("<li>").html(textEntry));
+		$(".textbox").val('');
+		});
+	$("ul").on("dblclick", function(event) {
+		event.preventDefault();
+		var index = $("ul").index(this);
+		$(this).remove();
+
+	})
+});
